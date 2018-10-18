56167cf9d290b1f91513b7dcfc2a7bcf0add57ec
Vasco Santos <vasco.santos@moxy.studio>
2018-10-18 15:34:49 +0100

    fix: first pass of code review

--- test/core/interface.spec.js
+++ test/core/interface.spec.js
@@ -72,13 +72,12 @@ describe('interface-ipfs-core tests', () => {
     }
   }))
 
-  /*
   tests.namePubsub(CommonFactory.create({
     spawnOptions: {
       args: ['--enable-namesys-pubsub'],
       initOptions: { bits: 1024 }
     }
-  })) */
+  }))
 
   tests.object(defaultCommonFactory)
 
